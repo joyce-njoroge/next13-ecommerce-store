8ddf3088703d106e6f6e87587bec2180c0f99e8d
Joyce Njoroge <joycewambuku33@gmail.com>
2023-08-28 12:48:00 +0300

    complete

--- ecommerce-store/app/(routes)/cart/components/summary.tsx
+++ ecommerce-store/app/(routes)/cart/components/summary.tsx
@@ -1,60 +1,63 @@
-"use client";
+// import { useEffect } from "react";
+// import { useSearchParams } from "next/navigation";
+// import { toast } from "react-hot-toast";
+// import useCart from "@/hooks/use-cart";
+// import Button from "@/components/ui/button";
+// import Currency from "@/components/ui/currency";
+// import axios from "axios";
 
-import axios from "axios";
-import { useEffect } from "react";
-import { useSearchParams } from "next/navigation";
+// const Summary = () => {
+//   const searchParams = useSearchParams();
+//   const items = useCart((state) => state.items);
+//   const removeAll = useCart((state) => state.removeAll);
 
-import Button from "@/components/ui/button";
-import Currency from "@/components/ui/currency";
-import useCart from "@/hooks/use-cart";
-import { toast } from "react-hot-toast";
+//   useEffect(() => {
+//     if (searchParams.get('success')) {
+//       toast.success('Payment completed.');
+//       removeAll();
+//     }
 
-const Summary = () => {
-  const searchParams = useSearchParams();
-  const items = useCart((state) => state.items);
-  const removeAll = useCart((state) => state.removeAll);
+//     if (searchParams.get('canceled')) {
+//       toast.error('Something went wrong.');
+//     }
+//   }, [searchParams, removeAll]);
 
-  useEffect(() => {
-    if (searchParams.get('success')) {
-      toast.success('Payment completed.');
-      removeAll();
-    }
+//   const totalPrice = items.reduce((total, item) => {
+//     return total + Number(item.price);
+//   }, 0);
 
-    if (searchParams.get('canceled')) {
-      toast.error('Something went wrong.');
-    }
-  }, [searchParams, removeAll]);
+//   const onCheckout = async () => {
+//     // Implement the M-Pesa payment initiation here
+//     try {
+//       const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/stk_initiate.php`, {
+//         productIds: items.map((item) => item.id),
+//       });
 
-  const totalPrice = items.reduce((total, item) => {
-    return total + Number(item.price)
-  }, 0);
+//       // Redirect the user to the M-Pesa payment URL
+//       if (response.data && response.data.url) {
+//         window.location = response.data.url;
+//       } else {
+//         toast.error('M-Pesa initiation failed.');
+//       }
+//     } catch (error) {
+//       toast.error('M-Pesa initiation failed.');
+//     }
+//   };
 
-  const onCheckout = async () => {
-    const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/checkout`, {
-      productIds: items.map((item) => item.id)
-    });
+//   return (
+//     <div className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
+//       <h2 className="text-lg font-medium text-gray-900">Order summary</h2>
+//       <div className="mt-6 space-y-4">
+//         <div className="flex items-center justify-between border-t border-gray-200 pt-4">
+//           <div className="text-base font-medium text-gray-900">Order total</div>
+//           <Currency value={totalPrice} />
+//         </div>
+//       </div>
+//       <Button onClick={onCheckout} disabled={items.length === 0} className="w-full mt-6">
+//         Checkout
+//       </Button>
+//     </div>
+//   );
+// };
 
-    window.location = response.data.url;
-  }
-
-  return ( 
-    <div
-      className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8"
-    >
-      <h2 className="text-lg font-medium text-gray-900">
-        Order summary
-      </h2>
-      <div className="mt-6 space-y-4">
-        <div className="flex items-center justify-between border-t border-gray-200 pt-4">
-          <div className="text-base font-medium text-gray-900">Order total</div>
-         <Currency value={totalPrice} />
-        </div>
-      </div>
-      <Button onClick={onCheckout} disabled={items.length === 0} className="w-full mt-6">
-        Checkout
-      </Button>
-    </div>
-  );
-}
- 
-export default Summary;
+// export default Summary;
